ee4de75b842616224f10ea124320476c74600955
Eden <edenshapiro@gmail.com>
2017-10-10 12:27:35 -0700

    Configure signing for push notifications

--- chatspot/AppDelegate.swift
+++ chatspot/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+		registerForPushNotifications()
         return true
     }
 
@@ -42,6 +44,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+	
+	func registerForPushNotifications() {
+  UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+	(granted, error) in
+	print("Permission granted: \(granted)")
+  }
+	}
 
 
 }
