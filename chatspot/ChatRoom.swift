8f48023221f313ba0350129a7591d40d2212df03
Varun Kochar <varunkochar08@gmail.com>
2017-10-12 23:43:28 -0700

    First Cut - Initializers for models, TODO: refactor the initializers based on needs.

--- chatspot/ChatRoom.swift
+++ chatspot/ChatRoom.swift
@@ -6,24 +6,58 @@
 //  Copyright © 2017 g7. All rights reserved.
 //
 
+import Realm
 import RealmSwift
 
 class ChatRoom: Object { // Object is from Realm, this also extends a NSObject.
     
     @objc dynamic var guid: String!
     @objc dynamic var name: String!
-    @objc dynamic var createdAt: Date!
+    @objc dynamic var createdAt: Double = 0
     @objc dynamic var roomBanner: String?
-
+    
     let longitude = RealmOptional<Double>() // primitive types can be made optional in realm by using a `RealmOptional`
     let latitude = RealmOptional<Double>()
     let userCount = RealmOptional<Int>()
     let roomActivity = RealmOptional<Int>()
-
+    
     // linking between a chat room and all of its messages
     let messages = LinkingObjects(fromType: Message.self, property: "chatRoom")
     
     override static func primaryKey() -> String? {
         return "guid"
+    }
+    
+    // our initializer with non-optional properties
+    convenience init(guid: String, createdAt: Double, name: String) {
+        self.init()
+        self.guid = guid
+        self.createdAt = createdAt
+        self.name = name
+    }
+    
+    // our initializer with all properties
+    convenience init(guid: String, createdAt: Double, name: String,
+                     roomBanner: String?, latitude: Double?, longitude: Double?, userCount: Int?, roomActivity: Int?) {
+        
+        self.init(guid: guid, createdAt: createdAt, name: name)
+        self.roomBanner = roomBanner
+        self.latitude.value = latitude
+        self.longitude.value = longitude
+        self.userCount.value = userCount
+        self.roomActivity.value = roomActivity
+    }
+    
+    // The required initializers
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
     }
 }
--- chatspot/User.swift
+++ chatspot/User.swift
@@ -6,13 +6,14 @@
 //  Copyright © 2017 g7. All rights reserved.
 //
 
+import Realm
 import RealmSwift
 
 class User: Object { // Object is from Realm, this also extends a NSObject.
     
     @objc dynamic var guid: String!
-    @objc dynamic var createdAt: Date!
-
+    let createdAt = RealmOptional<Double>()
+    
     @objc dynamic var name: String!
     @objc dynamic var tagline: String?
     @objc dynamic var profileImage: String?
@@ -24,5 +25,35 @@ class User: Object { // Object is from Realm, this also extends a NSObject.
     override static func primaryKey() -> String? {
         return "guid"
     }
-
+    
+    // our initializer with non-optional properties
+    convenience init(guid: String, createdAt: Double, name: String) {
+        self.init()
+        self.guid = guid
+        self.createdAt.value = createdAt
+        self.name = name
+    }
+    
+    // our initializer with all properties
+    convenience init(guid: String, createdAt: Double, name: String,
+                     tagline: String?, profileImage: String?, bannerImage: String?) {
+        
+        self.init(guid: guid, createdAt: createdAt, name: name)
+        self.tagline = tagline
+        self.profileImage = profileImage
+        self.bannerImage = bannerImage
+    }
+    
+    // The required initializers
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    required init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
+    }
 }
