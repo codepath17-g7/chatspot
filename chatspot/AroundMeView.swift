6af1715a0dc5bb95cbbd219a85b353a9ca0b95f6
Phuong Nguyen <phuong@itsoninc.com>
2017-10-15 02:03:54 -0700

    Add display of chatroom annotations

--- chatspot/AroundMeView.swift
+++ chatspot/AroundMeView.swift
@@ -13,6 +13,7 @@ class AroundMeView: UIView {
 
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var mapView: MKMapView!
+    var rooms: [ChatRoom1] = []
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,17 +35,37 @@ class AroundMeView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
 
+        mapView.delegate = self
         centerMap()
     }
     
     func updateRooms(_ rooms: [ChatRoom1]) {
-        
+        self.rooms = rooms
+        reloadMap()
     }
 
 }
 
+//MARK:- MKMapViewDelegate
 extension AroundMeView: MKMapViewDelegate {
     func reloadMap() {
+        
+        print("Reloading map with \(rooms.count) rooms")
+        mapView.removeAnnotations(mapView.annotations)
+
+        for room in rooms {
+            addAnnotationAtCoordinate(coordinate:
+                CLLocationCoordinate2D(latitude: room.latitude!, longitude: room.longitude!), title: room.name!)
+        }
+        
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
+    func addAnnotationAtCoordinate(coordinate: CLLocationCoordinate2D, title: String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = title
+        mapView.addAnnotation(annotation)
     }
     
     func centerMap() {
@@ -53,5 +74,32 @@ extension AroundMeView: MKMapViewDelegate {
         mapView.isZoomEnabled = true
         mapView.showsCompass = true
         
+    }
+    
+    func joinRoom(_ sender: Any) {
+        print("joining room")
+        
+        
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "chatroom"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            let button = UIButton(type: .detailDisclosure)
+            button.addTarget(self, action: #selector(joinRoom(_:)), for: .touchUpInside)
+
+            annotationView?.rightCalloutAccessoryView = button
+            annotationView?.canShowCallout = true
+            
+        } else {
+            annotationView?.annotation = annotation
+        }
+
+        annotationView?.image = #imageLiteral(resourceName: "around me")
+        return annotationView
     }
 }
