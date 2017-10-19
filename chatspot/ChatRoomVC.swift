dde463a14a150a4b8e2a2ea0842e5adeaea90f9b
Eden <edenshapiro@gmail.com>
2017-10-18 17:38:00 -0700

    Clean up ChatRoomVC code

--- chatspot/ChatRoomVC.swift
+++ chatspot/ChatRoomVC.swift
@@ -22,23 +22,24 @@ class ChatRoomVC: UIViewController, ChatMessageCellDelegate {
     @IBOutlet weak var chatRoomNameLabel: UILabel!
     @IBOutlet weak var chatRoomMemberCountLabel: UILabel!
     @IBOutlet weak var toolbarView: UIView!
-    
     @IBOutlet weak var roomImage: UIImageView!
-    
+    //Note: Footer will actually be the header once it has been transformed in viewDidLoad()
     @IBOutlet weak var footerView: UIView!
-    
     @IBOutlet weak var footerViewLabel: UILabel!
-
     
     @IBOutlet weak var containerTopMarginConstraint: NSLayoutConstraint!
     @IBOutlet weak var toolbarBottomConstraint: NSLayoutConstraint!
-	
+    
+    
+	var loadingMoreView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    var isMoreDataLoading = false
 	var messages: [Message1] = [Message1]()
     var chatRoom: ChatRoom1!
 	var initialY: CGFloat!
     var toolbarInitialY: CGFloat!
-    
     var observer: UInt!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 		tableView.delegate = self
@@ -50,8 +51,74 @@ class ChatRoomVC: UIViewController, ChatMessageCellDelegate {
         tableView.transform = CGAffineTransform(scaleX: 1, y: -1)
         footerView.transform = tableView.transform
         
+        // Set up the keyboard to move appropriately
 		setUpKeyboardNotifications()
+		
+        // UI setup
+        setUpUI()
         
+        // Heads Up Display
+//        setupAndTriggerHUD()
+        
+        // Infinite scrolling
+//        setUpInfiniteScrolling()
+        
+        // Load chatroom messages and start the observer
+        loadChatRoomMessages()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+        ChatSpotClient.removeObserver(handle: observer)
+    }
+    
+//MARK: ============ Initial Setup Methods ============
+
+    private func loadChatRoomMessages(){
+        ChatSpotClient.getMessagesForRoom(roomId: chatRoom.guid, success: { (messages: [Message1]) in
+            self.messages = messages
+            self.tableView.reloadData()
+            self.startObservingMessages()
+        }) { (e: Error?) in
+            print("Failure to load old messages: \(String(describing: e?.localizedDescription))")
+        }
+    }
+    
+    private func startObservingMessages() {
+        
+        observer = ChatSpotClient.observeNewMessages(roomId: chatRoom.guid, success: { (message: Message1) in
+            print(message)
+            self.messages.insert(message, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+        }, failure: {
+            print("Error in observeNewMessages")
+        })
+    }
+
+// TODO:
+//    func setUpInfiniteScrolling(){
+//        let tableFooterView: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 50))
+//        loadingMoreView.center = tableFooterView.center
+//        tableFooterView.insertSubview(loadingMoreView, at: 0)
+//        self.tableView.tableFooterView = tableFooterView
+//    }
+    
+// TODO:
+//    func setupAndTriggerHUD(){
+//        KRProgressHUD.set(style: .white)
+//        KRProgressHUD.set(font: .systemFont(ofSize: 17))
+//        KRProgressHUD.set(activityIndicatorViewStyle: .gradationColor(head: UIColor.ChatSpotColors.Blue, tail: UIColor.ChatSpotColors.DarkBlue))
+//        KRProgressHUD.show(withMessage: "Loading messages...")
+//    }
+	
+	func setUpUI(){
         chatRoomNameLabel.text = chatRoom.name
         footerViewLabel.text = chatRoom.name
         
@@ -74,55 +141,9 @@ class ChatRoomVC: UIViewController, ChatMessageCellDelegate {
             }
         }
         
-		
-		setUpUI()
-//        setupAndTriggerHUD()
-        self.loadChatRoomMessages()
-    }
-    private func loadChatRoomMessages(){
-        ChatSpotClient.getMessagesForRoom(roomId: chatRoom.guid, success: { (messages: [Message1]) in
-            self.messages = messages
-            self.tableView.reloadData()
-            self.startObservingMessages()
-        }) { (e: Error?) in
-            print("Failure to load old messages: \(e)")
-        }
-    }
-    
-    private func startObservingMessages() {
-        
-        observer = ChatSpotClient.observeNewMessages(roomId: chatRoom.guid, success: { (message: Message1) in
-            print(message)
-            self.messages.insert(message, at: 0)
-            let indexPath = IndexPath(row: 0, section: 0)
-            self.tableView.insertRows(at: [indexPath], with: .automatic)
-            self.tableView.reloadRows(at: [indexPath], with: .automatic)
-        }, failure: {
-            print("Error in observeNewMessages")
-        })
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.tabBarController?.tabBar.isHidden = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        self.tabBarController?.tabBar.isHidden = false
-        ChatSpotClient.removeObserver(handle: observer)
-    }
-    
-//    func setupAndTriggerHUD(){
-//        KRProgressHUD.set(style: .white)
-//        KRProgressHUD.set(font: .systemFont(ofSize: 17))
-//        KRProgressHUD.set(activityIndicatorViewStyle: .gradationColor(head: UIColor.ChatSpotColors.Blue, tail: UIColor.ChatSpotColors.DarkBlue))
-//        KRProgressHUD.show(withMessage: "Loading messages...")
-//    }
-	
-	func setUpUI(){
 		addPhotoButton.changeImageViewTo(color: .lightGray)
 		addEmojiButton.changeImageViewTo(color: .lightGray)
 		messageTextField.autoresizingMask = .flexibleWidth
-		
 	}
 	
 	func setUpKeyboardNotifications(){
@@ -161,17 +182,25 @@ class ChatRoomVC: UIViewController, ChatMessageCellDelegate {
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
-	
-    func presentAlertViewController(alertController: UIAlertController){
-        self.present(alertController, animated: true)
-    }
     
-    func sendPrivateMessageTo(userID: String){
+    func needToMoveContainerView(keyboardHeight: CGFloat) -> Bool {
+        var indexPath: IndexPath
+        if self.messages.count == 0 {
+            indexPath = IndexPath(row: 0, section: 0)
+        } else {
+            indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+        }
+        let frameForRow = self.tableView.rectForRow(at: indexPath)
+        let rectOfRowInSuperview = self.tableView.convert(frameForRow, to: self.tableView.superview)
+        if rectOfRowInSuperview.maxY > (self.view.frame.height - keyboardHeight){
+            return true
+        }
+        return false
     }
+
     
-    func viewUserProfile(userID: String){
-        
-    }
+//MARK: ============ User Interaction Methods ============
+
     
 	@IBAction func didTapAwayFromKeyboard(_ sender: UITapGestureRecognizer) {
 		view.endEditing(true)
@@ -195,24 +224,22 @@ class ChatRoomVC: UIViewController, ChatMessageCellDelegate {
         
     }
     
-    func needToMoveContainerView(keyboardHeight: CGFloat) -> Bool {
-        var indexPath: IndexPath
-        if self.messages.count == 0 {
-            indexPath = IndexPath(row: 0, section: 0)
-        } else {
-            indexPath = IndexPath(row: self.messages.count - 1, section: 0)
-        }
-        let frameForRow = self.tableView.rectForRow(at: indexPath)
-        let rectOfRowInSuperview = self.tableView.convert(frameForRow, to: self.tableView.superview)
-        if rectOfRowInSuperview.maxY > (self.view.frame.height - keyboardHeight){
-            return true
-        }
-        return false
+//MARK: ============ MessageCellDelegate Methods ============
+    
+    func presentAlertViewController(alertController: UIAlertController){
+        self.present(alertController, animated: true)
     }
+    
+    func sendPrivateMessageTo(userID: String){
+    }
+    
+    func viewUserProfile(userID: String){
+        
+    }
+    
 }
 
-
-// TableView Methods
+//MARK: ============ TableView Methods ============
 
 extension ChatRoomVC: UITableViewDelegate, UITableViewDataSource {
 	
@@ -233,6 +260,8 @@ extension ChatRoomVC: UITableViewDelegate, UITableViewDataSource {
 	}
 }
 
+//MARK: ============ Textfield and ImagePicker Methods ============
+
 extension ChatRoomVC: UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBAction func didTapAddPhoto(_ sender: AnyObject) {
@@ -251,7 +280,7 @@ extension ChatRoomVC: UITextFieldDelegate, UIImagePickerControllerDelegate, UINa
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
         picker.dismiss(animated: true, completion:nil)
-        
+        // TODO:
         // if it's a photo from the library, not an image from the camera
         //        if let referenceURL = info[UIImagePickerControllerReferenceURL] as? URL {
         
@@ -268,14 +297,45 @@ extension ChatRoomVC: UITextFieldDelegate, UIImagePickerControllerDelegate, UINa
     
 }
 
-extension ChatRoomVC: UIScrollViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        self.messageTextField.resignFirstResponder()
+//MARK: ============ ScrollView Methods ============
 
+extension ChatRoomVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                self.loadingMoreView.startAnimating()
+                loadMoreData()
+            }
+        }
+    }
+    
+    func loadMoreData() {
+// TODO:
+
+//        ChatSpotClient.
+//        TwitterClient.sharedInstance.homeTimeline(maxID: tweets[tweets.count - 1].id!, success: { (tweets: [Tweet]) in
+            self.isMoreDataLoading = false
+//            self.tweets.append(contentsOf: tweets.dropFirst())
+//            self.tableView.reloadData()
+            self.loadingMoreView.stopAnimating()
+//        }, failure: { (error: Error) in
+//            print("Could not find tweets: \(error.localizedDescription)")
+//            KRProgressHUD.set(font: .systemFont(ofSize: 15))
+//            KRProgressHUD.showError(withMessage: "Unable to load tweets.")
+//            self.isMoreDataLoading = false
+//            self.loadingMoreView.stopAnimating()
+//        })
     }
     
 }
+
+//MARK: ============ Object Extensions ============
 
 extension UIImageView {
 	func changeToColor(color: UIColor){
