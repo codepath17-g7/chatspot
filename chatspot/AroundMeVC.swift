3a0f551a8e291c1080609248a98d75718c71ad8d
Eden <edenshapiro@gmail.com>
2017-11-16 01:34:09 -0800

    Add back in AroundMe title

--- chatspot/AroundMeVC.swift
+++ chatspot/AroundMeVC.swift
@@ -19,7 +19,7 @@ class AroundMeVC: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.navigationItem.setUpTitle(title: "Around Me")
+        self.navigationItem.setUpTitle(title: "Around Me")
         self.aroundMeView.delegate = self
         setupSearchBar()
     }
@@ -70,8 +70,8 @@ class AroundMeVC: UIViewController, UISearchBarDelegate {
         if isClosing {
             leftConstraint.priority = UILayoutPriorityDefaultLow
             widthConstraint.priority = UILayoutPriorityDefaultHigh
-//            navigationItem.setUpTitle(title: "Around Me")
-//            navigationItem.leftBarButtonItem?.customView?.alpha = 0
+            navigationItem.setUpTitle(title: "Around Me")
+            navigationItem.leftBarButtonItem?.customView?.alpha = 0
 
         } else {
             leftConstraint.priority = UILayoutPriorityDefaultHigh
@@ -82,224 +82,20 @@ class AroundMeVC: UIViewController, UISearchBarDelegate {
         UIView.animate(withDuration: 2.0, animations: {
             // Switch the priorities to make search bar expand.
             self.searchBar.alpha = isClosing ? 0 : 1
-//            self.navigationItem.leftBarButtonItem?.customView?.alpha = isClosing ? 1 : 0
+            self.navigationItem.leftBarButtonItem?.customView?.alpha = isClosing ? 1 : 0
             self.searchBar.layoutIfNeeded()
-//            if !isClosing {
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//                    self.navigationItem.leftBarButtonItem = nil
-//                    UIView.animate(withDuration: 1.0, animations: {
-//                        self.searchBar.layoutIfNeeded()
-//                    })
-//                }
-//            }
+            if !isClosing {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    self.navigationItem.leftBarButtonItem = nil
+                    UIView.animate(withDuration: 1.0, animations: {
+                        self.searchBar.layoutIfNeeded()
+                    })
+                }
+            }
         },  completion: { (finished: Bool) in
 
         })
     }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //        let searchView = SearchView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: navigationController!.navigationBar.bounds.height))
-//        
-//        navigationItem.titleView = searchView
-//        
-//        
-//        searchView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        searchViewLeadingConstraint = NSLayoutConstraint(item: searchView, attribute: .leading, relatedBy: .equal, toItem: navigationItem.titleView, attribute: .leading, multiplier: 1, constant: 0)
-//        
-//        
-        
-        
-//        leftConstraint = NSLayoutConstraint(item: searchBar, attribute: .Left, relatedBy: .Equal, toItem: navigationItem.titleView, attribute: .LeftMargin, multiplier: 1, constant: 0)
-//                leftConstraint.priority = UILayoutPriorityDefaultLow // Starts out as low priority so it has no effect.
-//                navView.addConstraint(leftConstraint)
-//        
-//                navView.addConstraint(NSLayoutConstraint(item: navigationItem.titleView!, attribute: .right, relatedBy: .Equal, toItem: searchBar, attribute: .right, multiplier: 1, constant: 45))
-//                widthConstraint = NSLayoutConstraint(item: searchBar, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0, constant: 10)
-//                widthConstraint.priority = UILayoutPriorityDefaultHigh  // Starts out as high priority so it does have effect.
-//                navView.addConstraint(widthConstraint)
-//        
-//        
-//        let leadingConstraint = NSLayoutConstraint(item: smallDrawerView, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 0)
-//        let trailingConstraint = NSLayoutConstraint(item: smallDrawerView, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 0)
-//        
-//        view.addConstraints([leadingConstraint, trailingConstraint])
-
-        
-        
-        
-        
-        
-        
-        
-//        setupSearchBar()
-        
-        // searchbar background view
-//        searchBarView = UIView()
-//        searchBarView.backgroundColor = UIColor.ChatSpotColors.LighterGray
-//        searchBarView.translatesAutoresizingMaskIntoConstraints = false
-//        navigationItem.titleView = searchBarView
-//        
-//        // searchbar
-//        searchBar = UISearchBar(frame: CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>))
-//        searchBar.delegate = self
-//        searchBar.searchBarStyle = UISearchBarStyle.minimal
-//        
-//        searchBarView.addSubview(searchBar)
-        
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(openSearchBar))
-//        
-//    }
-//    
-//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-//        
-//    }
-//    
-//
-//    func openSearchBar(){
-//        
-//    }
-    
-//    class ViewController : UIViewController, UISearchBarDelegate {
-//        var searchBar = UISearchBar()
-//        var searchBarButtonItem: UIBarButtonItem?
-//        var logoImageView   : UIImageView!
-    
-//        override func viewDidLoad() {
-//            super.viewDidLoad()
-    
-            // Can replace logoImageView for titleLabel of navbar
-
-    
-//        
-//        @IBAction func searchButtonPressed(sender: AnyObject) {
-//            showSearchBar()
-//        }
-//        
-//        
-//        func openSearchBar() {
-//            searchBar.alpha = 0
-//            navigationItem.titleView = searchBar
-//            navigationItem.setLeftBarButtonItem(nil, animated: true)
-//            UIView.animateWithDuration(0.5, animations: {
-//                self.searchBar.alpha = 1
-//            }, completion: { finished in
-//                self.searchBar.becomeFirstResponder()
-//            })
-//        }
-//        
-//        func closeSearchBar() {
-//            navigationItem.setLeftBarButtonItem(searchBarButtonItem, animated: true)
-//            logoImageView.alpha = 0
-//            UIView.animateWithDuration(0.3, animations: {
-//                self.navigationItem.titleView = self.logoImageView
-//                self.logoImageView.alpha = 1
-//            }, completion: { finished in
-//                
-//            })
-//        }
-//        
-//        
-//        //MARK: UISearchBarDelegate
-//        func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-//            hideSearchBar()
-//        }
-    
-    
-    
-// FIXME: needs work
-//    func setupSearchBar(){
-    
-//        // Background view.
-//        searchView = UIView()
-//        searchView.backgroundColor = UIColor.ChatSpotColors.LighterGray
-//        searchView.translatesAutoresizingMaskIntoConstraints = false
-//        navigationItem.titleView = searchView
-//        
-//        searchView.superview!.addConstraint(NSLayoutConstraint(item: searchView, attribute: .top, relatedBy: .equal, toItem: searchView.superview!, attribute: .top, multiplier: 1, constant: 0))
-//        searchView.addConstraint(NSLayoutConstraint(item: searchView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: UIScreen.main.bounds.width))
-//        searchView.addConstraint(NSLayoutConstraint(item: searchView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: navigationController!.navigationBar.bounds.height))
-//        
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(openSearchBar))
-//        
-//        // Search bar.
-//        searchBar = UISearchBar()
-//        searchBar.translatesAutoresizingMaskIntoConstraints = false
-//        searchView.addSubview(searchBar)
-        
-//        navView.addConstraint(NSLayoutConstraint(item: searchBar, attribute: .CenterY, relatedBy: .Equal, toItem: navView, attribute: .CenterY, multiplier: 1, constant: 0))
-//        leftConstraint = NSLayoutConstraint(item: searchBar, attribute: .Left, relatedBy: .Equal, toItem: navigationItem.titleView, attribute: .LeftMargin, multiplier: 1, constant: 0)
-//        leftConstraint.priority = UILayoutPriorityDefaultLow // Starts out as low priority so it has no effect.
-//        navView.addConstraint(leftConstraint)
-//        
-//        navView.addConstraint(NSLayoutConstraint(item: navigationItem.titleView!, attribute: .Right, relatedBy: .Equal, toItem: searchBar, attribute: .Right, multiplier: 1, constant: 45))
-//        widthConstraint = NSLayoutConstraint(item: searchBar, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0, constant: 10)
-//        widthConstraint.priority = UILayoutPriorityDefaultHigh  // Starts out as high priority so it does have effect.
-//        navView.addConstraint(widthConstraint)
-//        
-        
-//        searchView.addConstraint(NSLayoutConstraint(item: searchBar, attribute: .centerY, relatedBy: .equal, toItem: searchView, attribute: .centerY, multiplier: 1, constant: 0))
-//        
-//        leftConstraint = NSLayoutConstraint(item: searchBar, attribute: .left, relatedBy: .equal, toItem: navigationItem.titleView, attribute: .leftMargin, multiplier: 1, constant: 0)
-//        
-//        leftConstraint.priority = UILayoutPriorityDefaultLow // Starts out as low priority so it has no effect.
-//        searchView.addConstraint(leftConstraint)
-//        
-//        searchView.addConstraint(NSLayoutConstraint(item: navigationItem.titleView!, attribute: .right, relatedBy: .equal, toItem: searchBar, attribute: .right, multiplier: 1, constant: 70))
-//        widthConstraint = NSLayoutConstraint(item: searchBar, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 0, constant: 10)
-//        widthConstraint.priority = UILayoutPriorityDefaultHigh  // Starts out as high priority so it does have effect.
-//        searchView.addConstraint(widthConstraint)
-//        
-//        // Remove search bar border.
-//        searchBar.layer.borderColor = UIColor.ChatSpotColors.LighterGray.cgColor
-//        searchBar.layer.borderWidth = 1
-//        
-//        // Match background color.
-//        searchBar.barTintColor = UIColor.ChatSpotColors.LighterGray
-//        
-//        // Search bar starts invisible.
-//        searchBar.alpha = 0
-//    }
-//    
-//    func openSearchBar() {
-//        
-//        let isClosing = leftConstraint.priority == UILayoutPriorityDefaultHigh
-//        
-//        // Switch the priorities to make search bar expand.
-//        if isClosing {
-//            leftConstraint.priority = UILayoutPriorityDefaultLow
-//            widthConstraint.priority = UILayoutPriorityDefaultHigh
-//            navigationItem.setUpTitle(title: "Around Me")
-//        } else {
-//            leftConstraint.priority = UILayoutPriorityDefaultHigh
-//            widthConstraint.priority = UILayoutPriorityDefaultLow
-//             navigationItem.leftBarButtonItem = nil
-//        }
-//        
-//        // Animate change to visible.
-//        UIView.animate(withDuration: 2, animations: {
-//            self.searchBar.alpha = isClosing ? 0 : 1
-//            self.searchBar.layoutIfNeeded()
-////            if let titleItem = self.navigationItem.leftBarButtonItem {
-////                self.navigationItem.leftBarButtonItem?.customView?.alpha = isClosing ? 1 : 0
-////            }
-//        })
-//    }
-
 }
 
 extension AroundMeVC: AroundMeViewDelegate {
